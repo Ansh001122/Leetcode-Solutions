921c3f825ac6d92277e6f43dbc38d49bf990ce16
Ansh <98034397+Ansh001122@users.noreply.github.com>
2026-07-09 01:27:53 +0530

    Time: 17 ms (84.47%), Space: 131 MB (24.27%) - LeetHub

--- 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.java
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.java
@@ -3,19 +3,16 @@ class Solution {
         int n = s.length();
         int MOD = 1_000_000_007;
 
-        // Prefix arrays for O(1) query retrieval
         long[] concat = new long[n];
         long[] prefix = new long[n];
         int[] nonZero = new int[n];
         long[] pow10 = new long[n];
 
-        // Precompute powers of 10
         pow10[0] = 1;
         for (int i = 1; i < n; i++) {
             pow10[i] = (pow10[i - 1] * 10) % MOD;
         }
 
-        // Build the prefix arrays
         long sum = 0;
         long val = 0;
         int cnt = 0;
@@ -31,7 +28,6 @@ class Solution {
             nonZero[i] = cnt;
         }
 
-        // Process all queries
         int m = queries.length;
         int[] res = new int[m];
         for (int i = 0; i < m; i++) {
@@ -41,17 +37,15 @@ class Solution {
             long temp1, temp2;
             if (l != 0) {
                 int count = nonZero[r] - nonZero[l - 1];
-                // Isolated window math with redundant mod operators removed
+                
                 temp1 = (concat[r] - (concat[l - 1] * pow10[count]) % MOD + MOD) % MOD;
                 temp2 = (prefix[r] - prefix[l - 1]) % MOD;
             } else {
                 temp1 = concat[r];
                 temp2 = prefix[r];
             }
-
             res[i] = (int) ((temp1 * temp2) % MOD);
         }
-
         return res;
     }
 }
