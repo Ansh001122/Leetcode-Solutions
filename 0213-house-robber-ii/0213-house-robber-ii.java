03e6539a93eeb4bcdc79eaa77c6f884b572b60ee
Ansh <98034397+Ansh001122@users.noreply.github.com>
2026-07-10 00:29:57 +0530

    Time: 0 ms (100%), Space: 43 MB (8.09%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -11,17 +11,19 @@ class Solution {
             skiplasthouse[i] = nums[i + 1];
         }    
 
-            int lootfirst = helpRob(skipfirsthouse);
-            int lootlast = helpRob(skiplasthouse);
+        int lootfirst = helpRob(skipfirsthouse);
+        int lootlast = helpRob(skiplasthouse);
 
-            return Math.max(lootfirst , lootlast);
+        return Math.max(lootfirst , lootlast);
     }
     public int helpRob(int[] nums){
         int n = nums.length;
-        if (n == 1) return nums[0];
+        if (n < 2) return nums[0];
+        
         int[] dp = new int[n];
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0] , nums[1]);
+
         for(int i = 2;i < n;i++){
             dp[i] = Math.max(dp[i-1] , dp[i-2] + nums[i]);
         }
