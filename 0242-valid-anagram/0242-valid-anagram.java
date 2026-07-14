534ea9c8640d322950df3838b5fb10d1150be3a4
Ansh <98034397+Ansh001122@users.noreply.github.com>
2026-07-15 02:01:31 +0530

    Time: 5 ms (61.44%), Space: 44.1 MB (89.81%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
+        if (s.length() != t.length()) {
             return false;
         }
-        char[] a = s.toCharArray();
-        char[] b = t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        if(Arrays.equals(a,b)){
-            return true;
+        int[] count = new int[26]; 
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']--; // just like we do for a number like '0' so for char we use'a'
+            count[t.charAt(i) - 'a']++; 
         }
-        else{
-            return false;
+        for (int val : count) {
+            if (val != 0) {
+                return false;
+            }
         }
+        return true;
     }
 }
