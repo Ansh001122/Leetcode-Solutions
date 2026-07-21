bfad408fd67b0d5bd9d25facb1aed2196ec9a8f6
Ansh <98034397+Ansh001122@users.noreply.github.com>
2026-07-22 00:50:21 +0530

    Time: 79 ms (71.83%), Space: 47.5 MB (95.77%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
@@ -1,40 +1,34 @@
 class Solution {
     public int maxActiveSectionsAfterTrade(String s) {
-        int n = s.length();
         int totalOnes = 0;
-        
-        // Collect lengths and types of consecutive blocks
-        java.util.List<Integer> blockLens = new java.util.ArrayList<>();
-        java.util.List<Character> blockTypes = new java.util.ArrayList<>();
+        int maxZeroMerge = 0;
+        int prevZeroLen = 0;
         
         int i = 0;
+        int n = s.length();
+        
         while (i < n) {
             int j = i;
             while (j < n && s.charAt(j) == s.charAt(i)) {
                 j++;
             }
+            
             int len = j - i;
-            blockLens.add(len);
-            blockTypes.add(s.charAt(i));
             
             if (s.charAt(i) == '1') {
                 totalOnes += len;
+            } else {
+                // If there was a '0' block before the current one (separated by a '1' block),
+                // prevZeroLen will be > 0, allowing them to merge.
+                if (prevZeroLen > 0) {
+                    maxZeroMerge = Math.max(maxZeroMerge, prevZeroLen + len);
+                }
+                prevZeroLen = len;
             }
+            
             i = j;
         }
         
-        int maxTradeGain = 0;
-        
-        // Find internal '1' blocks surrounded by '0' blocks on both sides
-        int numBlocks = blockTypes.size();
-        for (int k = 1; k < numBlocks - 1; k++) {
-            if (blockTypes.get(k) == '1' && blockTypes.get(k - 1) == '0' && blockTypes.get(k + 1) == '0') {
-                int leftZeroLen = blockLens.get(k - 1);
-                int rightZeroLen = blockLens.get(k + 1);
-                maxTradeGain = Math.max(maxTradeGain, leftZeroLen + rightZeroLen);
-            }
-        }
-        
-        return totalOnes + maxTradeGain;
+        return totalOnes + maxZeroMerge;
     }
 }
