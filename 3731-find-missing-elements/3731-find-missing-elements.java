11e6c355493f8e03875919e9b84ebaa04545be5e
Ansh <98034397+Ansh001122@users.noreply.github.com>
2026-08-05 01:44:28 +0530

    Time: 3 ms (81.9%), Space: 46.9 MB (42.26%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
-      ArrayList<Integer> ans = new ArrayList<>();
+     /* ArrayList<Integer> ans = new ArrayList<>();
       Arrays.sort(nums);
       int n = nums.length;
       int prev = nums[0];
@@ -9,6 +9,26 @@ class Solution {
             ans.add(++prev);
         }
         prev = nums[i];
+      }
+      return ans;*/
+
+
+      HashSet<Integer> set = new HashSet<>();
+
+      int max = Integer.MIN_VALUE;
+      int min = Integer.MAX_VALUE;
+
+      for(int x : nums){
+        set.add(x);
+        min = Math.min(min , x);
+        max = Math.max(max , x);
+      }
+      List<Integer> ans = new ArrayList<>();
+
+      for(int i = min; i <= max;i++){
+        if(!set.contains(i)){
+            ans.add(i);
+        }
       }
       return ans;
     }
