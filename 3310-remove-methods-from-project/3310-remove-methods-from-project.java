6fd5c2164fdec2f0e54989393d1c14401d6576af
Ansh <98034397+Ansh001122@users.noreply.github.com>
2026-08-06 01:33:57 +0530

    Time: 52 ms (94.55%), Space: 285.1 MB (55.45%) - LeetHub

--- 3310-remove-methods-from-project/3310-remove-methods-from-project.java
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -1,42 +1,45 @@
 class Solution {
     public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
-        List<List<Integer>> graph = new ArrayList<>();
+       List<List<Integer>> edges = new ArrayList<>(n);
         for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
+            edges.add(new ArrayList<>());
         }
+        int[] inDegree = new int[n];
         for (int[] inv : invocations) {
-            graph.get(inv[0]).add(inv[1]);
+            edges.get(inv[0]).add(inv[1]);
+            inDegree[inv[1]]++;
+        }
+        
+        Queue<Integer> queue = new ArrayDeque<>();
+        boolean[] suspicious = new boolean[n];
+        queue.offer(k);
+        suspicious[k] = true;
+
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+            for (int v : edges.get(u)) {
+                inDegree[v]--;
+                if (!suspicious[v]) {
+                    suspicious[v] = true;
+                    queue.offer(v);
+                }
+            }
         }
 
-        boolean[] isSuspicious = new boolean[n];
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(k);
-        isSuspicious[k] = true;
-        
-        while (!queue.isEmpty()) {
-            int curr = queue.poll();
-            for (int neighbor : graph.get(curr)) {
-                if (!isSuspicious[neighbor]) {
-                    isSuspicious[neighbor] = true;
-                    queue.offer(neighbor);
+        for (int i = 0; i < n; i++) {
+            if (suspicious[i] && inDegree[i] > 0) {
+                // Return all nodes from 0 to n-1 if removal is unsafe
+                List<Integer> allNodes = new ArrayList<>(n);
+                for (int j = 0; j < n; j++) {
+                    allNodes.add(j);
                 }
+                return allNodes;
             }
         }
-        for (int[] inv : invocations) {
-            int u = inv[0];
-            int v = inv[1];
-            if (!isSuspicious[u] && isSuspicious[v]) {
-                List<Integer> allMethods = new ArrayList<>(n);
-                for (int i = 0; i < n; i++) {
-                    allMethods.add(i);
-                }
-                return allMethods;
-            }
-        }
-        
+      
         List<Integer> remaining = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            if (!isSuspicious[i]) {
+            if (!suspicious[i]) {
                 remaining.add(i);
             }
         }
