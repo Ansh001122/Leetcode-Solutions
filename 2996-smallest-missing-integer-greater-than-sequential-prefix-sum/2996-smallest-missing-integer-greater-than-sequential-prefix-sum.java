b12647a560ae066b0532148484e5c9b0ab7d2769
Ansh <98034397+Ansh001122@users.noreply.github.com>
2026-08-15 03:05:16 +0530

    Time: 0 ms (100%), Space: 43.9 MB (73.17%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.java
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -1,18 +1,20 @@
 class Solution {
     public int missingInteger(int[] nums) {
+        boolean hash[] = new boolean[51];
+        
+        for(int i = 0;i < nums.length;i++){
+            hash[nums[i]] = true;
+        }
         int sum = nums[0];
-        for (int i = 1; i < nums.length; i++) {//
-            if (nums[i] == nums[i - 1] + 1) {
-                sum += nums[i];//Taken from the question itself
-            } else {
-                break; // Sequential prefix broken
+        for(int i = 0;i < nums.length-1;i++){
+            if(nums[i] + 1 == nums[i + 1]){
+                sum += nums[i+1];
+            }
+            else {
+                break;
             }
         }
-        Set<Integer> set = new HashSet<>();//set for handling the numbers sequence wise 
-        for (int num : nums) {
-            set.add(num);
-        }
-        while (set.contains(sum)) { //if the set contains the sum then return it otherwise ++ and give the higher after number
+        while(sum <= 50 && hash[sum]){
             sum++;
         }
         return sum;
