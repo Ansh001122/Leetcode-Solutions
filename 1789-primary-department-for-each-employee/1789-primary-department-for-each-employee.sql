a14f064d54eb37216c444f31e2f412519ac48628
Ansh <98034397+Ansh001122@users.noreply.github.com>
2026-08-16 01:02:20 +0530

    Time: 589 ms (56.45%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -6,16 +6,9 @@ FROM
     Employee
 WHERE 
     primary_flag = 'Y'
-
-UNION
-
--- Case 2: Employee belongs to only 1 department
-SELECT 
-    employee_id, 
-    department_id
-FROM 
-    Employee
-GROUP BY 
-    employee_id
-HAVING 
-    COUNT(department_id) = 1;
+    OR employee_id IN (
+        SELECT employee_id
+        FROM Employee
+        GROUP BY employee_id
+        HAVING COUNT(department_id) = 1
+    );
