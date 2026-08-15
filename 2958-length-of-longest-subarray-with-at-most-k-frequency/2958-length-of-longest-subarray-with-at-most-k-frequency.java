9fbd0cdff0859c306fd01692691cb2449a76134b
Ansh <98034397+Ansh001122@users.noreply.github.com>
2026-08-16 00:56:01 +0530

    Time: 67 ms (53.36%), Space: 88.9 MB (71.77%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
@@ -1,18 +1,18 @@
 class Solution {
     public int maxSubarrayLength(int[] nums, int k) {
-     HashMap<Integer, Integer> map = new HashMap<>();
+     HashMap<Integer, Integer> map = new HashMap<>();//map for storing frequencies
         int result = 0, i = 0, j = 0, n = nums.length;
         
         while (j < n) {
-            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
+            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);//put in map the frequencies till j<n
             
-            while (map.get(nums[j]) > k) {
+            while (map.get(nums[j]) > k) {//1st condition for shrinking the window size 
                 map.put(nums[i], map.get(nums[i]) - 1);
-                i++;
+                i++;//then again adding the element to the window
             }
             
-            result = Math.max(result, j - i + 1);
-            j++;
+            result = Math.max(result, j - i + 1);//2nd condition normal way to finding the current window size in sliding window
+            j++;//getting the final window
         }
         
         return result;
