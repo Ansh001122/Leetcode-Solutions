6a760a16e9cd893bdc4c32cbf35861c4c9c9e02e
Ansh <98034397+Ansh001122@users.noreply.github.com>
2026-08-16 01:01:52 +0530

    Time: 572 ms (66.52%), Space: 0B (100%) - LeetHub

---
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -0,0 +1,21 @@
+# Write your MySQL query statement below
+SELECT 
+    employee_id, 
+    department_id
+FROM 
+    Employee
+WHERE 
+    primary_flag = 'Y'
+
+UNION
+
+-- Case 2: Employee belongs to only 1 department
+SELECT 
+    employee_id, 
+    department_id
+FROM 
+    Employee
+GROUP BY 
+    employee_id
+HAVING 
+    COUNT(department_id) = 1;
