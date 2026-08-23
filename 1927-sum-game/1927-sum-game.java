bd5e4761445e19950a1331e812ac3b129c96cce4
Ansh <98034397+Ansh001122@users.noreply.github.com>
2026-08-24 01:27:14 +0530

    Time: 8 ms (53.24%), Space: 47.3 MB (10.79%) - LeetHub

--- 1927-sum-game/1927-sum-game.java
+++ 1927-sum-game/1927-sum-game.java
@@ -1,27 +1,16 @@
 class Solution {
     public boolean sumGame(String num) {
-        int n = num.length();
-        int sumDiff = 0;
-        int qDiff = 0;
+       int n = num.length();
+        int balance = 0; // Tracks 2 * sum + 9 * '?'
 
         for (int i = 0; i < n; i++) {
-            int sign = (i < n / 2) ? 1 : -1;
-            char ch = num.charAt(i);
-
-            if (ch == '?') {
-                qDiff += sign;
-            } else {
-                sumDiff += sign * (ch - '0');
-            }
+            char c = num.charAt(i);
+            int val = (c == '?') ? 9 : 2 * (c - '0');
+            
+            // First half adds, second half subtracts
+            balance += (i < n / 2) ? val : -val;
         }
 
-        // If net question mark difference is odd, Alice makes the last move and wins
-        if ((qDiff & 1) != 0) {
-            return true;
-        }
-
-        // Bob can only win if the sum difference perfectly offsets Bob's 9-sum pairing strategy
-        // Alice wins if 2 * sumDiff + qDiff * 9 != 0
-        return (2 * sumDiff + qDiff * 9) != 0;
+        return balance != 0;
     }
 }
