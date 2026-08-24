96e27db1b2df9dd7fee5fe44b01d3826af44dd52
Ansh <98034397+Ansh001122@users.noreply.github.com>
2026-08-24 23:37:23 +0530

    Time: 2 ms (99.49%), Space: 44.8 MB (86.33%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> nextGreaterMap = new HashMap<>();
+       /* Map<Integer, Integer> nextGreaterMap = new HashMap<>();
         Deque<Integer> stack = new ArrayDeque<>();
         
         for (int num : nums2) {
@@ -13,6 +13,27 @@ class Solution {
         for (int i = 0; i < nums1.length; i++) {
             result[i] = nextGreaterMap.getOrDefault(nums1[i], -1);
         }
-        return result;
+        return result;*/
+
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int[] res = new int[n1];
+        
+        Arrays.fill(res, -1);
+        for (int i = 0; i < n1; i++) {
+            int target = nums1[i];
+            int j = 0;
+            
+            while (j < n2 && nums2[j] != target) {
+                j++;
+            }
+            for (int k = j + 1; k < n2; k++) {
+                if (nums2[k] > target) {
+                    res[i] = nums2[k];
+                    break;
+                }
+            }
+        }
+        return res;
     }
 }
