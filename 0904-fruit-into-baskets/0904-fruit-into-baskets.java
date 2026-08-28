2656669f61bb07ec2610423c4feccf7a0338b869
Ansh <98034397+Ansh001122@users.noreply.github.com>
2026-08-29 00:01:44 +0530

    Time: 54 ms (66.64%), Space: 70.8 MB (18.15%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -3,26 +3,21 @@ class Solution {
         Map<Integer, Integer> fruitCount = new HashMap<>();
         int left = 0;
         int maxFruits = 0;
-
         for (int right = 0; right < fruits.length; right++) {
             // Add fruit at right to window
             fruitCount.put(fruits[right], fruitCount.getOrDefault(fruits[right], 0) + 1);
-
             // If we have more than 2 distinct fruit types, shrink from left
             while (fruitCount.size() > 2) {
                 int leftFruit = fruits[left];
                 fruitCount.put(leftFruit, fruitCount.get(leftFruit) - 1);
-                
                 if (fruitCount.get(leftFruit) == 0) {
                     fruitCount.remove(leftFruit);
                 }
                 left++;
             }
-
             // Update maximum window size
             maxFruits = Math.max(maxFruits, right - left + 1);
         }
-
         return maxFruits;
     }
 }
