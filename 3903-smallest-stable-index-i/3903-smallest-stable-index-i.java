3627ceccb1a983c8cf7442f7892d77ab42eff3a9
Ansh <98034397+Ansh001122@users.noreply.github.com>
2026-09-06 00:21:16 +0530

    Time: 1 ms (99.79%), Space: 46.5 MB (32.63%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -1,26 +1,19 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
         int n = nums.length;
-        
-        // sufMin[i] stores the minimum element in nums[i..n-1]
         int[] sufMin = new int[n];
         sufMin[n - 1] = nums[n - 1];
         for (int i = n - 2; i >= 0; i--) {
             sufMin[i] = Math.min(nums[i], sufMin[i + 1]);
         }
-
         int prefMax = Integer.MIN_VALUE;
-
         // Find the smallest stable index
         for (int i = 0; i < n; i++) {
             prefMax = Math.max(prefMax, nums[i]);
-            
-            // Instability score: max(nums[0..i]) - min(nums[i..n-1])
             if (prefMax - sufMin[i] <= k) {
                 return i;
             }
         }
-
         return -1;
     }
 }
