16558831a79654e431fa791a0db164f8f028234d
Ansh <98034397+Ansh001122@users.noreply.github.com>
2026-09-17 00:32:38 +0530

    Time: 36 ms (59.35%), Space: 173.3 MB (5.81%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
@@ -2,8 +2,6 @@ class Solution {
     public int numberOfSets(int n, int k) {
         int MOD = 1_000_000_007;
         int maxN = n + k;
-        
-        // Build Pascal's Triangle to compute combinations modulo 10^9 + 7
         long[][] C = new long[maxN + 1][maxN + 1];
         
         for (int i = 0; i <= maxN; i++) {
@@ -12,8 +10,6 @@ class Solution {
                 C[i][j] = (C[i - 1][j - 1] + C[i - 1][j]) % MOD;
             }
         }
-        
-        // The answer is C(n + k - 1, 2k)
         return (int) C[n + k - 1][2 * k];
     }
 }
