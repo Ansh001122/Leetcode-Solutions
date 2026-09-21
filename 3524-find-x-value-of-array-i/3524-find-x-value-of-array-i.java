28b0c4f49015cb6fd8622a4e08ba8e8270e30c95
Ansh <98034397+Ansh001122@users.noreply.github.com>
2026-09-22 01:45:40 +0530

    Time: 14 ms (67.31%), Space: 93.4 MB (50%) - LeetHub

--- 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.java
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.java
@@ -1,31 +1,31 @@
 class Solution {
     public long[] resultArray(int[] nums, int k) {
        long[] ans = new long[k];
-       long[] dp = new long[k]; // dp[r] tracks counts of subarrays ending at current index with remainder r
+        // dp[r] := the number of subarrays ending at current position with product % k == r
+        long[] dp = new long[k];
         
-        for (int num : nums) {
+        for (final int num : nums) {
             long[] newDp = new long[k];
-            int numMod = num % k;
+            final int numMod = num % k;
             
-            // 1. Start a fresh subarray consisting only of the current element
-            newDp[numMod]++;
+            // 1. Start a new subarray with only `num`.
+            newDp[numMod] = 1;
             
-            // 2. Extend all previous subarrays ending at the prior position
-            for (int i = 0; i < k; i++) {
-                if (dp[i] > 0) {
-                    int newMod = (int) ((1L * i * numMod) % k);
-                    newDp[newMod] += dp[i];
-                }
+            // 2. Extend all previous subarrays.
+            for (int i = 0; i < k; ++i) {
+                final int newMod = (int) (1L * i * numMod % k);
+                newDp[newMod] += dp[i];
             }
             
-            // 3. Accumulate results into the final answer array
-            for (int i = 0; i < k; i++) {
+            // 3. Accumulate counts into the final answer array.
+            for (int i = 0; i < k; ++i) {
                 ans[i] += newDp[i];
             }
             
-            dp = newDp; // Roll forward to the next index
+            // 4. Roll over state for the next iteration.
+            dp = newDp;
         }
         
-        return ans; 
+        return ans;
     }
 }
