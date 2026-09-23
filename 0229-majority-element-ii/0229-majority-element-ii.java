3a1412eeee6c77a15b55923797384ff8a04435e8
Ansh <98034397+Ansh001122@users.noreply.github.com>
2026-09-24 01:58:56 +0530

    Time: 4 ms (56.43%), Space: 52.3 MB (79.17%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -0,0 +1,47 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        Integer cand1 = null, cand2 = null;
+        int count1 = 0, count2 = 0;
+
+        // Step 1: Find potential majority candidates
+        for (int num : nums) {
+            if (cand1 != null && num == cand1) {
+                count1++;
+            } else if (cand2 != null && num == cand2) {
+                count2++;
+            } else if (count1 == 0) {
+                cand1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                cand2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        // Step 2: Verify the actual frequencies of the candidates
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (cand1 != null && num == cand1) {
+                count1++;
+            } else if (cand2 != null && num == cand2) {
+                count2++;
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int threshold = nums.length / 3;
+        
+        if (cand1 != null && count1 > threshold) {
+            result.add(cand1);
+        }
+        if (cand2 != null && count2 > threshold && !cand2.equals(cand1)) {
+            result.add(cand2);
+        }
+
+        return result;
+    }
+}
