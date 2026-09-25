b2225d7e239765db04aac373ff1a1a106e48fe4e
Ansh <98034397+Ansh001122@users.noreply.github.com>
2026-09-26 02:12:42 +0530

    Time: 2 ms (93.84%), Space: 45.9 MB (32.9%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
         findCombinations(candidates, target, 0, new ArrayList<>(), result);
@@ -16,5 +16,27 @@ class Solution {
         findCombinations(candidates, target - candidates[index], index, current, result);
         current.remove(current.size() - 1); // Backtrack
         findCombinations(candidates, target, index + 1, current, result);
+    }
+}*/
+
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(candidates, target, 0, new ArrayList<>(), result);
+        return result;
+    }
+    private void backtrack(int[] candidates, int remain, int startIndex, List<Integer> currentPath, List<List<Integer>> result) {
+        if (remain == 0) {
+            result.add(new ArrayList<>(currentPath));
+            return;
+        }
+        if (remain < 0) {
+            return;
+        }
+        for (int i = startIndex; i < candidates.length; i++) {
+            currentPath.add(candidates[i]);
+            backtrack(candidates, remain - candidates[i], i, currentPath, result);
+            currentPath.remove(currentPath.size() - 1);
+        }
     }
 }
